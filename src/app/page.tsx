c12c0edd086544fe1ddd7c6426655033682ae042
Aran Jaiman <aranjaiman@outlook.com>
2024-03-05 15:11:37 -0500

    increased size of title text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,10 @@ export default function Home() {
       <div className="trap1"></div>
       <div className="absolute top-0 left-0 pt-[15px] pl-[50px] pr-[30px] w-full">
         <Navbar />
-        <div className="flex gap-[30rem] mt-20">
+        <div className="flex justify-between border border-black mt-20">
           <section className="px-2">
-            <h1 className="text-3xl font-bold">Linkedin.</h1>
-            <h1 className="text-3xl font-bold">For<span className="text-orange-600">&nbsp;High&nbsp;</span>Schoolers.</h1>
+            <h1 className="text-6xl font-bold">Linkedin.</h1>
+            <h1 className="text-6xl font-bold">For<span className="text-orange-600">&nbsp;High&nbsp;</span>Schoolers.</h1>
             <p className="py-5 text-lg max-w-80 text-wrap"><span className="text-orange-600">Find&nbsp;</span>Internships.<span className="text-orange-600">&nbsp;Find&nbsp;</span>Programs.<span className="text-orange-600">&nbsp;Connect&nbsp;</span>with peers. Made simple with<span className="italic">&nbsp;Internia.&nbsp;</span></p>
           </section>
           <section>
