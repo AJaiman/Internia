9d05fd4d8107a4c7901e96c417d30e3a5519b6e2
Aran Jaiman <aranjaiman@outlook.com>
2024-03-05 15:21:15 -0500

    adjusted side margins for elements in the first section

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -4,7 +4,7 @@ import Searchbar from "./searchbar";
 
 export default function Navbar() {
     return (
-        <nav className="flex h-10 justify-between p-2">
+        <nav className="flex h-10 justify-between py-2 px-20">
             <h1 className="text-xl">Monkey Type</h1>
             <section className="flex items-center gap-2">
                 <Searchbar />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <>
     {/* Parent of Section 1 */}
-    <div className="relative">
+    <div className="relative border border-black">
       {/* Bacground of Section 1 */}
       <div className="trap1"></div>
       <div className="absolute top-0 left-0 pt-[15px] pl-[50px] pr-[30px] w-full">
         <Navbar />
-        <div className="flex justify-between border border-black mt-20">
-          <section className="px-2">
+        <div className="flex justify-between border border-black mt-20 px-20">
+          <section className="border border-black">
             <h1 className="text-6xl font-bold">Linkedin.</h1>
             <h1 className="text-6xl font-bold">For<span className="text-orange-600">&nbsp;High&nbsp;</span>Schoolers.</h1>
             <p className="py-5 text-lg max-w-80 text-wrap"><span className="text-orange-600">Find&nbsp;</span>Internships.<span className="text-orange-600">&nbsp;Find&nbsp;</span>Programs.<span className="text-orange-600">&nbsp;Connect&nbsp;</span>with peers. Made simple with<span className="italic">&nbsp;Internia.&nbsp;</span></p>
