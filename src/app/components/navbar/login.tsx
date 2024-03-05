2ff3678ba725a1c5feee554b4027b6f6331b2223
AJaiman <aravjaiman@outlook.com>
2024-03-05 12:08:05 -0500

    Added info text

--- src/app/components/navbar/login.tsx
+++ src/app/components/navbar/login.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function LoginSignUp() {
     return (
         <div className="flex items-center gap-8 border-l-2 border-gray-400 pl-6 ml-7">
-            <button className="hover:text-orange-400 transition duration-200">Join Now</button>
-            <button className="border-2 drop-shadow-md text-orange-400 border-orange-400 py-2 px-4 rounded-full hover:bg-orange-400 hover:text-white transition duration-200">Sign Up</button>
+            <button className="hover:text-orange-600 transition duration-200">Join Now</button>
+            <button className="border-2 drop-shadow-md text-orange-600 border-orange-600 py-2 px-4 rounded-full hover:bg-orange-600 hover:text-white transition duration-200">Sign Up</button>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,20 @@ import Navbar from "./components/navbar/navbar";
 export default function Home() {
   return (
     <>
-    <div className="trap1">
-      <Navbar />
+    <div className="border border-black">
+      <div className="trap1">
+        <Navbar />
+        <div className="flex gap-[30rem] mt-20">
+          <section className="px-2 border border-black">
+            <h1 className="text-3xl font-bold">Linkedin.</h1>
+            <h1 className="text-3xl font-bold">For<span className="text-orange-600">&nbsp;High&nbsp;</span>Schoolers.</h1>
+            <p className="py-5 text-lg max-w-80 text-wrap"><span className="text-orange-600">Find&nbsp;</span>Internships.<span className="text-orange-600">&nbsp;Find&nbsp;</span>Programs.<span className="text-orange-600">&nbsp;Connect&nbsp;</span>with peers. Made simple with<span className="italic">&nbsp;Internia.&nbsp;</span></p>
+          </section>
+          <section>
+            <div className="border border-black bg-gray size-96 z-1"></div>
+          </section>
+        </div>
+      </div>
     </div>
     </>
   );
