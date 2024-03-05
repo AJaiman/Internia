1b08ea6a89c329c94b733993855e5be40b483284
AJaiman <aravjaiman@outlook.com>
2024-03-05 12:47:07 -0500

    Added proper positioning of elements on top of the trapezoid and added box for image but image is not showing up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,20 @@ import Navbar from "./components/navbar/navbar";
 export default function Home() {
   return (
     <>
-    <div className="border border-black">
-      <div className="trap1">
+    {/* Parent of Section 1 */}
+    <div className="relative">
+      {/* Bacground of Section 1 */}
+      <div className="trap1"></div>
+      <div className="absolute top-0 left-0 pt-[15px] pl-[50px] pr-[30px] w-full">
         <Navbar />
         <div className="flex gap-[30rem] mt-20">
-          <section className="px-2 border border-black">
+          <section className="px-2">
             <h1 className="text-3xl font-bold">Linkedin.</h1>
             <h1 className="text-3xl font-bold">For<span className="text-orange-600">&nbsp;High&nbsp;</span>Schoolers.</h1>
             <p className="py-5 text-lg max-w-80 text-wrap"><span className="text-orange-600">Find&nbsp;</span>Internships.<span className="text-orange-600">&nbsp;Find&nbsp;</span>Programs.<span className="text-orange-600">&nbsp;Connect&nbsp;</span>with peers. Made simple with<span className="italic">&nbsp;Internia.&nbsp;</span></p>
           </section>
           <section>
-            <div className="border border-black bg-gray size-96 z-1"></div>
+            <img className="border border-black size-96 rounded-lg" src="src/app/placeholderimage.png" alt="Nun" />
           </section>
         </div>
       </div>
