1ee2c2568167a6da98cf0fa256177c993db09d3d
AJaiman <aravjaiman@outlook.com>
2024-03-05 16:22:16 -0500

    Updated title message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
         <Navbar />
         <div className="flex justify-between border border-black mt-20 px-20">
           <section className="border border-black">
-            <h1 className="text-6xl font-bold">Linkedin.</h1>
-            <h1 className="text-6xl font-bold">For<span className="text-orange-600">&nbsp;High&nbsp;</span>Schoolers.</h1>
+            <h1 className="text-6xl font-bold">A <span className="text-orange-600">&nbsp;Network</span></h1>
+            <h1 className="text-6xl font-bold">For High Schoolers.</h1>
             <p className="py-5 text-lg max-w-80 text-wrap"><span className="text-orange-600">Find&nbsp;</span>Internships.<span className="text-orange-600">&nbsp;Find&nbsp;</span>Programs.<span className="text-orange-600">&nbsp;Connect&nbsp;</span>with peers. Made simple with<span className="italic">&nbsp;Internia.&nbsp;</span></p>
           </section>
           <section>
