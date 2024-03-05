797b72d15977f4dbb4750c41f0b64628437f5d19
AJaiman <aravjaiman@outlook.com>
2024-03-05 16:44:09 -0500

    Added explore now button and fixed some spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/navbar";
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 export default function Home() {
   return (
     <>
     {/* Parent of Section 1 */}
-    <div className="relative border border-black">
+    <div className="relative">
       {/* Bacground of Section 1 */}
       <div className="trap1"></div>
       <div className="absolute top-0 left-0 pt-[15px] pl-[50px] pr-[30px] w-full">
         <Navbar />
-        <div className="flex justify-between border border-black mt-20 px-20">
-          <section className="border border-black">
-            <h1 className="text-6xl font-bold">A <span className="text-orange-600">&nbsp;Network</span></h1>
-            <h1 className="text-6xl font-bold">For High Schoolers.</h1>
+        <div className="flex justify-between mt-28 px-20">
+          <section className="flex flex-col gap-3">
+            <div>
+              <h1 className="text-7xl font-bold">A <span className="text-orange-600">&nbsp;Network</span></h1>
+              <h1 className="text-7xl font-bold">For High Schoolers.</h1>
+            </div>
             <p className="py-5 text-lg max-w-80 text-wrap"><span className="text-orange-600">Find&nbsp;</span>Internships.<span className="text-orange-600">&nbsp;Find&nbsp;</span>Programs.<span className="text-orange-600">&nbsp;Connect&nbsp;</span>with peers. Made simple with<span className="italic">&nbsp;Internia.&nbsp;</span></p>
+            <button className="max-w-fit flex items-center rounded-full text-white bg-orange-600 py-2 pl-4 pr-3 hover:bg-orange-400 transition duration-200">Explore Now<MdKeyboardArrowRight className="text-xl"/></button>
           </section>
           <section>
-            <img className="border border-black size-96 rounded-lg" src="src/app/placeholderimage.png" alt="Nun" />
+            <img className="size-96 rounded-lg" src="../public/placeholderimage.png" alt="Nun" />
           </section>
         </div>
       </div>
