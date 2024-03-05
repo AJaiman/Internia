f0507bbf5c9a6fe6e86ef843aa488a8d9c14bb37
AJaiman <aravjaiman@outlook.com>
2024-03-05 11:19:37 -0500

    Updated styling for the search bar

--- src/app/components/navbar/searchbar.tsx
+++ src/app/components/navbar/searchbar.tsx
@@ -3,8 +3,8 @@ import { IoIosSearch } from "react-icons/io";
 
 export default function Searchbar() {
     return (
-        <div className="flex gap-0 drop-shadow-md">
-            <input type="text" placeholder="Search" className="pl-4 py-2 pr-2 rounded-l-full border-l border-t border-b border-black focus:outline-none"/>
+        <div className="flex gap-0 drop-shadow-md customWidth">
+            <input type="text" placeholder="Search" className="pl-4 py-2 pr-2 rounded-l-full border-l border-t border-b border-black w-full focus:outline-none"/>
             <button className="bg-white border-r border-t border-b border-black pr-3 rounded-r-full text-xl">
                 <IoIosSearch />
             </button>
