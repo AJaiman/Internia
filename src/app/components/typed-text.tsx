b953ca1911630e9ce5760f795b617184857f53fe
AJaiman <aravjaiman@outlook.com>
2024-03-07 11:05:19 -0500

    Added gap between lines of typewriter

--- src/app/components/typed-text.tsx
+++ src/app/components/typed-text.tsx
@@ -6,23 +6,23 @@ import { Typewriter } from "react-simple-typewriter";
 export default function TypedText() {
 
     return (
-        <h1 className="text-5xl flex flex-col">
+        <h1 className="text-5xl flex flex-col gap-2">
             <div>
                 <span className="text-orange-600 font-bold">Find</span> <span className="italic">your&nbsp;</span>
                 <span><Typewriter 
                 words={['interests.']}
-                cursor
+                
                 /></span>
             </div>
             <div className="ml-[15.70vw]"><Typewriter 
             words={['', 'internships.']}
-            cursor
+            
             delaySpeed={900}
             
             /></div>
             <div className="ml-[15.70vw]"><Typewriter 
             words={['', 'oppurtunities.']}
-            cursor
+            
             delaySpeed={2000}
             /></div>
         </h1>
