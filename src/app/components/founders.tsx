a96bc91a77caaf9ca0fe4f4ac96cd9494dae5e8f
AJaiman <aravjaiman@outlook.com>
2024-03-11 15:13:08 -0400

    Added info for founders

--- src/app/components/founders.tsx
+++ src/app/components/founders.tsx
@@ -3,14 +3,17 @@ import FounderIcon from "./founder-icon";
 
 export default function Founders() {
     
-    const aravInfo = 'Arav Jaiman is a student who loves to code and get busy/rizzy'
+    const aravInfo = 'Arav is a student who loves to code and keep himself busy.'
+    const ericInfo ='Eric was either PG for the Mavs or a developer for Internia.'
+    const sehajInfo = "Internia was Sehaj's brain-child. He designed it into reality."
+    const rohithInfo = 'Rohith was the guy we needed for sillyness XD.'
 
     return (
         <div className="flex gap-[3vw]">
-            <FounderIcon style={'founderSlideInAnim1'} color={'#FFE1E1'} info={aravInfo} pfpsrc={'/icons/chip.png'} name={'Eric Huang'}/>
-            <FounderIcon style={'founderSlideInAnim2'} color={'#FFE1E1'} info={aravInfo} pfpsrc={'/icons/chip.png'} name={'Eric Huang'}/>
-            <FounderIcon style={'founderSlideInAnim3'} color={'#FFE1E1'} info={aravInfo} pfpsrc={'/icons/chip.png'} name={'Eric Huang'}/>
-            <FounderIcon style={'founderSlideInAnim4'} color={'#FFE1E1'} info={aravInfo} pfpsrc={'/icons/chip.png'} name={'Eric Huang'}/>
+            <FounderIcon style={'founderSlideInAnim1'} color={'#FFE1E1'} info={ericInfo} pfpsrc={'/icons/chip.png'} name={'Eric Huang'}/>
+            <FounderIcon style={'founderSlideInAnim2'} color={'#FFE1E1'} info={aravInfo} pfpsrc={'/icons/chip.png'} name={'Arav Jaiman'}/>
+            <FounderIcon style={'founderSlideInAnim3'} color={'#FFE1E1'} info={sehajInfo} pfpsrc={'/icons/chip.png'} name={'Sehaj Singh'}/>
+            <FounderIcon style={'founderSlideInAnim4'} color={'#FFE1E1'} info={rohithInfo} pfpsrc={'/icons/chip.png'} name={'Rohith Athrey'}/>
         </div>
     )
 }
