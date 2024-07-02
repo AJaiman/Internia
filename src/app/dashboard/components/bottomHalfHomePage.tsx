3bf6a29360f689c9fcb006baa43f39856b990c31
Arav <aravjaiman@outlook.com>
2024-07-02 17:12:30 -0400

    Created slider componenet

--- src/app/dashboard/components/bottomHalfHomePage.tsx
+++ src/app/dashboard/components/bottomHalfHomePage.tsx
@@ -1,19 +1,10 @@
 "use client"
 import React, { useState } from "react";
+import Slider from "./slider";
 
 export default function BottomHalfHomePage() {
-    const [rangeValue, setRangeValue] = useState(50);
-
-    const handleRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRangeValue(Number(event.target.value));
-      };
+    
     return (
-        <div className="flex flex-col items-center">
-            <h1>Readability</h1>
-            <span className="flex items-center">
-                <div className="w-11 m-4">{rangeValue}%</div>
-                <input type="range" min="0" max="100" defaultValue='50' onChange={handleRangeChange} className="w-[40vw] range [--range-shdw:green] range-lg border border-black bg-[#D9D9D9]" />
-            </span>
-        </div>
+        <Slider name='Readability'/>
     )
 }
---
+++ src/app/dashboard/components/slider.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+type SliderProps = {
+    name: string,
+}
+
+export default function Slider(props: SliderProps) {
+    const [rangeValue, setRangeValue] = useState(50);
+
+    const handleRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRangeValue(Number(event.target.value));
+      };
+      
+    return (
+        <div className="flex flex-col items-center">
+            <h1>{props.name}</h1>
+            <span className="flex items-center">
+                <div className="w-11 m-4">{rangeValue}%</div>
+                <input type="range" min="0" max="100" defaultValue='50' onChange={handleRangeChange} className="w-[40vw] range [--range-shdw:green] range-lg border border-black bg-[#D9D9D9]" />
+            </span>
+        </div>
+    )
+}
