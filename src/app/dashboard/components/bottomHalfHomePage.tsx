21159602a4e470fb16682dfc67ab0851f9661049
Arav <aravjaiman@outlook.com>
2024-07-02 22:17:32 -0400

    Finished bottom half of dashboard main page

--- src/app/dashboard/components/bottomHalfHomePage.tsx
+++ src/app/dashboard/components/bottomHalfHomePage.tsx
@@ -5,6 +5,13 @@ import Slider from "./slider";
 export default function BottomHalfHomePage() {
     
     return (
-        <Slider name='Readability'/>
+        <>
+            <Slider name='Interest'/>
+            <Slider name='Readability'/>
+            <div className="w-full flex justify-between px-40 mt-[7vh]">
+                <button className="bg-[#8A0000] hover:bg-[#680000] text-white px-5 py-2 rounded-full text-2xl font-bold">Skip</button>
+                <button className="bg-[#002ABD] hover:bg-[#001B7B] text-white px-5 py-2 rounded-full text-2xl font-bold">Confirm</button>
+            </div>
+        </>
     )
 }
--- src/app/dashboard/components/slider.tsx
+++ src/app/dashboard/components/slider.tsx
@@ -13,10 +13,10 @@ export default function Slider(props: SliderProps) {
       
     return (
         <div className="flex flex-col items-center">
-            <h1>{props.name}</h1>
+            <h1 className="font-bold">{props.name}</h1>
             <span className="flex items-center">
                 <div className="w-11 m-4">{rangeValue}%</div>
-                <input type="range" min="0" max="100" defaultValue='50' onChange={handleRangeChange} className="w-[40vw] range [--range-shdw:green] range-lg border border-black bg-[#D9D9D9]" />
+                <input type="range" min="0" max="100" defaultValue='50' onChange={handleRangeChange} className="w-[40vw] range [--range-shdw:teal] range-lg border border-black bg-[#D9D9D9]" />
             </span>
         </div>
     )
