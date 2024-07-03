440c9e0d53196673f7d1053ce1772ee9f6c7bb1c
Bryan Yung <byung806@gmail.com>
2024-07-03 17:43:27 -0400

    Add top half of dashboard & combine

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,17 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import BottomHalfHomePage from "./components/bottomHalfHomePage";
+import TopHalfHomePage from "./components/topHalfHomePage";
 
 const page = async () => {
   const session = await getServerSession(authOptions);
   console.log(session);
   
   return (
-    <></>
+    <div className="space-y-10">
+        <TopHalfHomePage />
+        <BottomHalfHomePage />
+    </div>
   )
 }
 
