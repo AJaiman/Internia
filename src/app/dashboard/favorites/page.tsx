ed67f4f2e345ca3c0531fdd88bc8963faf93f9af
Arav <aravjaiman@outlook.com>
2024-07-13 13:38:57 -0400

    finished favorites page

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+"use client"
 
-export default function FavoritesPage() {
+import React, { useEffect, useRef, useState } from "react";
+import DashNav from "../components/dashNav";
+import CondensedProfCard from "../components/profsAndPapersList/condensedProfCard";
+import CondensedPaperCard from "../components/profsAndPapersList/condensedPaperCard";
+
+export default function HistoryPage() {
+
+    const profTab = useRef<HTMLInputElement>(null);
+    const paperTab = useRef<HTMLInputElement>(null);
+
+    const [profFocus, setProfFocus] = useState(true);
+    const [paperFocus, setPaperFocus] = useState(false);
+
+
+
+    const handleProfFocus = () => {
+        if (!profFocus) {
+            setProfFocus(true);
+            setPaperFocus(false);
+        }
+    }
+    const handlePaperFocus = () => {
+        if (!paperFocus) {
+            setPaperFocus(true);
+            setProfFocus(false);
+        }
+    }
+
+    useEffect(() => {
+
+        if (profFocus) {
+            profTab.current?.focus();
+        }
+        else {
+            paperTab.current?.focus();
+        }
+
+    }, [profFocus, paperFocus])
+
+    const professorHistory = ["Dr. Kefter-Oobleck", "Dr. Kefter-Oobleck", "Dr. Kefter-Oobleck", "Dr. Kefter-Oobleck", "Dr. Kefter-Oobleck", "Dr. Kefter-Oobleck", "Dr. Kefter-Oobleck", "Dr. Kefter-Oobleck"]
+    const paperHistory = ["Rizzology: The Study of Riss and its Effects on Human Nature", "Rizzology: The Study of Riss and its Effects on Human Nature", "Rizzology: The Study of Riss and its Effects on Human Nature", "Rizzology: The Study of Riss and its Effects on Human Nature", "Rizzology: The Study of Riss and its Effects on Human Nature", "Rizzology: The Study of Riss and its Effects on Human Nature"]
+
+
     return (
-        <><p>Test</p></>
+        <>
+        <DashNav pfp=""/>
+        <div role="tablist" className="tabs mt-10 px-5">
+            <input ref={profTab} type="radio" onFocus={handleProfFocus}  name="my_tabs_1" role="tab" className="tab focus:drop-shadow-lg border-b-4 border-slate focus:border-b-4 transition duration-200" aria-label="Professors" />
+
+            <input ref={paperTab} type="radio" onFocus={handlePaperFocus} name="my_tabs_1" role="tab" className="tab focus:drop-shadow-lg border-b-4 border-slate focus:border-b-4 transition duration-200" aria-label="Papers" />
+        </div>
+        <div className="p-10">
+            <div className="overflow-auto h-[72vh]">
+                {
+                    profFocus ?
+                    professorHistory.map(prof => (
+                        <CondensedProfCard profId={prof} />
+                    )) : 
+                    
+                    paperHistory.map(paper => (
+                        <CondensedPaperCard paperId={paper} />
+                    ))
+                }
+            </div>
+        </div>
+        </>
     )
 }
