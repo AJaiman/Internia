6dc36a4947a644f883aa232b13920a9dd4360e89
Bryan Yung <byung806@gmail.com>
2024-07-16 22:02:04 -0400

    Add numbersOnly option to text box

--- frontend/src/app/signUpPage/components/signUpLoginTextBox.tsx
+++ frontend/src/app/signUpPage/components/signUpLoginTextBox.tsx
@@ -5,16 +5,28 @@ interface SignUpLoginTextBoxProps {
     placeholder?: string;
     value?: string;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    numbersOnly?: boolean;
 }
 
-export default function SignUpLoginTextBox({ type="text", placeholder="", value="", onChange, ...props }: SignUpLoginTextBoxProps & any) {
+export default function SignUpLoginTextBox({ type="text", placeholder="", value="", onChange, numbersOnly, ...props }: SignUpLoginTextBoxProps & any) {
+    function _onChange(e: React.ChangeEvent<HTMLInputElement>) {
+        if (numbersOnly) {
+            const re = /^[0-9\b]+$/;
+            if (e.target.value === '' || re.test(e.target.value)) {
+                onChange(e);
+            }
+        } else {
+            onChange(e);
+        }
+    }
+    
     return (
         <input
             {...props}
             type={type}
             placeholder={placeholder}
             value={value}
-            onChange={onChange}
+            onChange={_onChange}
             className={`text-2xl px-6 py-2 text-gray-900 font-bold bg-[#d9d9d9] rounded-full transition duration-200 ${props.className}`}
         />
     );
