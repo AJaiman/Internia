7b5aa209fab8cd3ef9dbece6ae387805b7f9845d
Shom770 <shayaanwadkar@gmail.com>
2024-11-01 16:21:52 -0400

    add border to publication card

--- frontend/src/app/ui/professor/publication-card.tsx
+++ frontend/src/app/ui/professor/publication-card.tsx
@@ -11,12 +11,12 @@ export default function PublicationCard({ publication } : { publication: Publica
 
     return (
         <div className="flex flex-row w-full h-20">
-            <div className="flex flex-col justify-center pl-4 flex-1 h-20 bg-white/80 rounded-l-[6px]">
+            <div className="flex flex-col justify-center pl-4 flex-1 h-20 bg-white/80 rounded-l-[6px] border border-royalPurple/30 border-r-0">
                 <h1 className="text-base font-bold text-royalPurple">{publicationName}</h1>
                 <h1 className="text-xs text-royalPurple">{authors}</h1>
                 <h1 className="text-xs text-royalPurple">{publication.yearPublished}</h1>
             </div>
-            <Link href="/discover/paper/owueroiweur" className="group flex items-center justify-center w-20 h-20 rounded-r-[6px] bg-white/50">
+            <Link href="/discover/paper/owueroiweur" className="group flex items-center justify-center w-20 h-20 rounded-r-[6px] bg-white/50 border border-royalPurple/30 border-l-0">
                 <ArrowRightIcon className="w-8 h-8 text-royalPurple transition group-hover:scale-125" />
             </Link>
         </div>
