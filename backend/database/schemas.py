0f34b6e44d960c34d432a49d97e791f7dc4d4757
AravJ <aravjaiman@outlook.com>
2024-11-22 20:23:02 -0500

    updated getting user data

--- backend/database/schemas.py
+++ backend/database/schemas.py
@@ -1,5 +1,6 @@
 def single_user(user):
     return {
+        "id": str(user["_id"]),
         "first_name": user["first_name"],
         "last_name": user["last_name"],
         "email": user["email"],
