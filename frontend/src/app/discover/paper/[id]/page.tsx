b70bcadc3fcdbe1e979d96c866ab7e3e847f66a4
Arav <aravjaiman@outlook.com>
2024-11-23 23:07:55 -0500

    Added simple pdf viewer for testing purposes

--- frontend/src/app/discover/paper/[id]/page.tsx
+++ frontend/src/app/discover/paper/[id]/page.tsx
@@ -16,16 +16,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
                     fullDate: "June 2022",
                     link: "https://doi.org/oweariouwerio",
                     abstract: "Many people have eaten Chipotle and became obesity shortly after. Unfortunately, obesity is an epidemic and the spread of Chipotle is concerning. This paper dives into how fat people who eat Chipotle are: Chipotle is very bad for the human body. Do not eat chiptotle",
-                    content: `
-                    Lorem ipsum odor amet, consectetuer adipiscing elit. Congue velit nascetur blandit aptent nisi. Dictum auctor ac ullamcorper fames porta. Metus tellus imperdiet est taciti nisl. Ornare laoreet urna dictumst varius interdum dolor ligula. Elit facilisi ac donec ipsum sagittis cursus. Nostra magnis cras potenti per nisl tincidunt. Dolor consectetur gravida mi congue leo fames ultrices senectus. Hendrerit sagittis lectus massa purus penatibus; etiam adipiscing litora primis? Eu habitasse velit urna ex arcu fusce erat taciti.
-
-                    Penatibus torquent eget nisi aenean habitant ridiculus diam. Dictum lorem erat hendrerit molestie aenean metus elementum? Ante tristique inceptos etiam porta vulputate nascetur odio. Elit quisque nisi elit placerat nisl ad. Cubilia suscipit litora dolor lobortis penatibus cras. Natoque enim natoque commodo ultricies ligula fames laoreet torquent finibus! Nostra sit fermentum nullam donec morbi malesuada luctus. Vehicula diam ex lacus nisi luctus blandit morbi. Dui class congue conubia facilisis integer consectetur ante ultrices.
-
-                    Lorem ipsum odor amet, consectetuer adipiscing elit. Congue velit nascetur blandit aptent nisi. Dictum auctor ac ullamcorper fames porta. Metus tellus imperdiet est taciti nisl. Ornare laoreet urna dictumst varius interdum dolor ligula. Elit facilisi ac donec ipsum sagittis cursus. Nostra magnis cras potenti per nisl tincidunt. Dolor consectetur gravida mi congue leo fames ultrices senectus. Hendrerit sagittis lectus massa purus penatibus; etiam adipiscing litora primis? Eu habitasse velit urna ex arcu fusce erat taciti.
-
-Penatibus torquent eget nisi aenean habitant ridiculus diam. Dictum lorem erat hendrerit molestie aenean metus elementum? Ante tristique inceptos etiam porta vulputate nascetur odio. Elit quisque nisi elit placerat nisl ad. Cubilia suscipit litora dolor lobortis penatibus cras. Natoque enim natoque commodo ultricies ligula fames laoreet torquent finibus! Nostra sit fermentum nullam donec morbi malesuada luctus. Vehicula diam ex lacus nisi luctus blandit morbi. Dui class congue conubia facilisis integer consectetur ante ultrices.
-
-                    `,
+                    content: "https://www.aclweb.org/anthology/N18-3011.pdf",
                     match: 0.98
                 }}
                 isRecommended={false} />
--- frontend/src/app/ui/paper/paper-content.tsx
+++ frontend/src/app/ui/paper/paper-content.tsx
@@ -1,7 +1,7 @@
 export default function PaperContent({ content } : { content: string }) {
     return (
-        <div className="w-full h-full overflow-y-auto border border-royalPurple/20 rounded-[6px] p-8">
-            <p className="text-sm text-royalPurple">{content}</p>
+        <div className="w-full h-full overflow-y-auto border border-royalPurple/20 rounded-[6px]">
+            <iframe src={content} className="w-full h-full" />
         </div>
     )
 }
