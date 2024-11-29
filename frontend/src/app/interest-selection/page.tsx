c45a45caa469b93b6c49c29e6cb19bfe7db94de9
Arav <aravjaiman@outlook.com>
2024-11-28 22:36:28 -0500

    Put field selection component on field selection page

--- frontend/src/app/interest-selection/page.tsx
+++ frontend/src/app/interest-selection/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import FieldSelector from "../ui/field-selector";
 
 export default function InterestSelection() {
     return (
         <>
-        
+        <FieldSelector />
         </>
     );
 }
