ad7d1fe8855fd7af39e52da299bd9c279333e29f
Arav <aravjaiman@outlook.com>
2025-02-09 18:14:09 -0500

    No more non english papers

--- backend/papers.py
+++ backend/papers.py
@@ -1,10 +1,17 @@
 import requests
-
+from langdetect import detect
 paper_info_url = "http://api.semanticscholar.org/graph/v1/paper/"
 paper_batch_info_url = "https://api.semanticscholar.org/graph/v1/paper/batch"
 paper_recommendations_url = "http://api.semanticscholar.org/recommendations/v1/papers/"
 
 # api_key = "1234567890" TODO: get api key
+
+def is_english(text):
+    try:
+        return detect(text) == "en"
+    except:
+        return False
+
 
 def get_paper_info(paper_id: str):
     queryParams = {"fields": "title,abstract,authors,isOpenAccess,openAccessPdf,year,externalIds,publicationDate,url"}
@@ -23,7 +30,7 @@ def get_paper_batch_info(paper_ids: list[str]):
         return response.json()
     
 def get_paper_recs(positive_papers: list[str], negative_papers: list[str]):
-    queryParams = {"limit": 500, "fields": "openAccessPdf"}
+    queryParams = {"limit": 500, "fields": "openAccessPdf,title"}
     response = requests.post(
         paper_recommendations_url,
         json={
@@ -40,7 +47,7 @@ def get_paper_recs(positive_papers: list[str], negative_papers: list[str]):
     if "recommendedPapers" in response_data:
         open_access_papers = [
             paper for paper in response_data["recommendedPapers"] 
-            if paper["openAccessPdf"] is not None
+            if paper["openAccessPdf"] is not None and is_english(paper["title"])
         ]
         response_data["recommendedPapers"] = open_access_papers
         response = {"recommendedPapers": [paper["paperId"] for paper in response_data["recommendedPapers"]]}
